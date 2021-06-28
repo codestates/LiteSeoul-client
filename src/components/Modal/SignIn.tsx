47565dfebae7c3643141df6cf4725a2feecbad87
basilry <basilry@gmail.com>
2021-06-29 01:12:47 +0900

    [Client] feat/Signup add: Validation and format

--- src/components/Modal/SignIn.tsx
+++ src/components/Modal/SignIn.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function SignIn(){
-    return(
-        <div>
-            signin
-        </div>
-    )
+function SignIn() {
+  return (
+    <div className="SignInNav">
+      SignIn
+      <ul>
+        <li>
+          <NavLink exact to="/signup">
+            SignUp
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SignIn;
