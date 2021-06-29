b3242a8a5a5333fc948e3cfae70be37691cca855
basilry <basilry@gmail.com>
2021-06-29 17:59:21 +0900

    [Client] feat/ranking add: components

--- src/components/ranking/ShopRank.tsx
+++ src/components/ranking/ShopRank.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
-function ShopRank(){
-    return(
-        <div>
-            shoprank
-        </div>
-    )
+function ShopRank() {
+  return (
+    <div>
+      shoprank
+      <ul>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+        <li>샵랭킹</li>
+      </ul>
+    </div>
+  );
 }
 
 export default ShopRank;
