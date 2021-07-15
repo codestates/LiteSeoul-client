8ae56cf717330df6a01de0452c9308eca55fc14b
basilry <basilry@gmail.com>
2021-07-15 15:54:20 +0900

    final client plz

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -64,7 +64,7 @@ function Home({ handleModal, handleModalData, isLogin }: any) {
       })
       .then((res: any) => {
         localStorage.setItem('recommend', JSON.stringify(res.data));
-        console.log(JSON.parse(localStorage.getItem('recommend') || '{}'));
+        // console.log(JSON.parse(localStorage.getItem('recommend') || '{}'));
       });
   }, [isLogin]);
 
